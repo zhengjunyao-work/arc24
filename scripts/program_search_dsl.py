9ccdc596da7349a1361eb27dae818606ff965bb7
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2024-08-16 12:51:25 +0200

    removed icecuber solution from program search

--- scripts/program_search_dsl.py
+++ scripts/program_search_dsl.py
@@ -5,8 +5,6 @@ https://www.kaggle.com/code/mehrankazeminia/3-arc24-developed-2020-winning-solut
 
 import os
 import json
-import time
-from subprocess import Popen, PIPE, STDOUT
 import numpy as np
 import pandas as pd
 from collections import defaultdict
@@ -27,13 +25,10 @@ def main(args=None):
         args = sys.argv[1:]
     args = parse_args(args)
     compile_feature_extraction_for_sklearn_tree()
-    run_icecube_solver(
-        dataset_filepath=args.dataset_filepath, solution_filepath='sub_icecube.json')
-    sub_solver = run_main_solvers(
-        dataset_filepath=args.dataset_filepath,
-        icecuber_solution_filepath='sub_icecube.json')
+    submission = run_main_solvers(
+        dataset_filepath=args.dataset_filepath)
     with open(args.output_filepath, 'w') as file:
-        json.dump(sub_solver, file, indent=4)
+        json.dump(submission, file, indent=4)
 
 
 def parse_args(args):
@@ -2227,101 +2222,6 @@ def flattener(pred):
     str_pred = str_pred.replace(']]', '|')
     return str_pred
 
-"""Icecube"""
-def adapt_arc24_files_to_arc20_format(json_file_path, output_dir):
-    # Load the JSON content
-    with open(json_file_path, 'r') as file:
-        data = json.load(file)
-
-    # Create the 'test' directory
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Split the JSON content into individual files
-    for task_id, task_data in tqdm(data.items(), total=len(data), desc='Splitting tasks into multiple files'):
-        output_file_path = os.path.join(output_dir, f'{task_id}.json')
-        with open(output_file_path, 'w') as output_file:
-            json.dump(task_data, output_file, indent=4)
-
-
-def mySystem(cmd):
-    print(cmd)
-    t0 = time.time()
-    process = Popen(cmd, stdout=PIPE, stderr=STDOUT, shell=True)
-    for line in iter(process.stdout.readline, b''):
-        print(line.decode("utf-8"), end='')
-    assert(process.wait() == 0)
-    print(f'{time.time() - t0:.1f} seconds took to run {cmd}')
-
-
-def translate_submission_from_old_csv_format_to_new_json_format(input_filepath, output_filepath):
-    # Read the original submission file
-    with open(input_filepath, 'r') as file:
-        lines = file.readlines()
-
-    submission_dict = {}
-
-    for line in lines[1:]:  # Skip the header line
-        output_id, output = line.strip().split(',')
-        task_id, output_idx = output_id.split('_')
-        predictions = output.split(' ')  # Split predictions based on ' '
-
-        # Take only the first two predictions
-        if len(predictions) > 2:
-            predictions = predictions[:2]
-
-        processed_predictions = []
-        for pred in predictions:
-            if pred:  # Check if pred is not an empty string
-                pred_lines = pred.split('|')[1:-1]  # Remove empty strings from split
-                pred_matrix = [list(map(int, line)) for line in pred_lines]
-                processed_predictions.append(pred_matrix)
-
-        attempt_1 = processed_predictions[0] if len(processed_predictions) > 0 else []
-        attempt_2 = processed_predictions[1] if len(processed_predictions) > 1 else []
-
-        if task_id not in submission_dict:
-            submission_dict[task_id] = []
-
-        attempt_dict = {
-            "attempt_1": attempt_1,
-            "attempt_2": attempt_2
-        }
-
-        if output_idx == '0':
-            submission_dict[task_id].insert(0, attempt_dict)
-        else:
-            submission_dict[task_id].append(attempt_dict)
-
-    # Write to the new json file
-    with open(output_filepath, 'w') as file:
-        json.dump(submission_dict, file, indent=4)
-
-
-def check_icecube_dataset_has_correct_version():
-    if open("../input/arc-solution-source-files-by-icecuber/version.txt").read().strip() == "671838222":
-        print("Dataset has correct version")
-    else:
-        print("Dataset version not matching!")
-        assert(0)
-
-
-def run_icecube_solver(dataset_filepath, solution_filepath):
-    print("\n\n\t\tRunning Icecube solver")
-    adapt_arc24_files_to_arc20_format(
-        json_file_path=dataset_filepath,
-        output_dir='/kaggle/working/abstraction-and-reasoning-challenge/test')
-    check_icecube_dataset_has_correct_version()
-    mySystem("cp -r ../input/arc-solution-source-files-by-icecuber ./absres-c-files")
-    mySystem("cd absres-c-files; make -j")
-    mySystem("cd absres-c-files; python3 safe_run.py")
-    mySystem("cp absres-c-files/submission_part.csv old_icecube_submission.csv")
-    mySystem("tar -czf store.tar.gz absres-c-files/store")
-    mySystem("rm -r absres-c-files")
-    translate_submission_from_old_csv_format_to_new_json_format(
-        '/kaggle/working/old_icecube_submission.csv', solution_filepath)
-    print("\t\tIcecube solver completed\n\n")
-
-
 """Run solvers"""
 def ganswer_answer(ganswer):
 
@@ -2384,18 +2284,17 @@ def create_dummy_submission(data):
     submission = {}
     for task_id, task in data.items():
         submission[task_id] = []
-        for i in range(len(task['test'])):
-            submission[task_id].append({
-                'attempt_1': [[0, 0], [0, 0]],
-                'attempt_2': [[0, 0], [0, 0]]
-            })
+        for _ in range(len(task['test'])):
+            # submission[task_id].append({
+            #     'attempt_1': [[0, 0], [0, 0]],
+            #     'attempt_2': [[0, 0], [0, 0]]
+            # })
+            submission[task_id].append({'attempt_1': [],'attempt_2': []})
     return submission
 
 
-def run_main_solvers(dataset_filepath, icecuber_solution_filepath):
+def run_main_solvers(dataset_filepath):
     print("\n\n\t\tRunning main solvers")
-    with open(icecuber_solution_filepath , 'r') as f:
-        sub_icecube = json.load(f)
     with open(dataset_filepath,'r') as f:
         data = json.load(f)
     submission = create_dummy_submission(data)
@@ -2486,22 +2385,6 @@ def run_main_solvers(dataset_filepath, icecuber_solution_filepath):
                 submission[task_id][i]['attempt_1'] = prn[0]
                 if (len(prn)==2):
                     submission[task_id][i]['attempt_2'] = prn[1]
-
-            # ............................................................................... 5 - ICECube
-            if (submission[task_id][i]['attempt_1'] != [[0, 0], [0, 0]]):
-                if (sub_icecube[task_id][i]['attempt_1'] != [[0, 0], [0, 0]]):
-
-                    if (submission[task_id][i]['attempt_1'] != sub_icecube[task_id][i]['attempt_1']):
-                        submission[task_id][i]['attempt_2'] =  sub_icecube[task_id][i]['attempt_1']
-            else:
-                if (submission[task_id][i]['attempt_2'] == [[0, 0], [0, 0]]): #I have the intuition this is always true
-
-                    if (sub_icecube[task_id][i]['attempt_1'] != [[0, 0], [0, 0]]):
-                        submission[task_id][i]['attempt_1'] = sub_icecube[task_id][i]['attempt_1']
-
-                    if (sub_icecube[task_id][i]['attempt_2'] != [[0, 0], [0, 0]]):
-                        submission[task_id][i]['attempt_2'] = sub_icecube[task_id][i]['attempt_2']
-
     return submission
 
 
