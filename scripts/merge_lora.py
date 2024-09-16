ba5727c3382b032b4c6a73bc9602cf59a5302249
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2024-09-16 16:03:19 +0200

    updated merge lora script to work also when lora is not used

--- scripts/merge_lora.py
+++ scripts/merge_lora.py
@@ -7,6 +7,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 import shutil
 
+from arc24.logging import logging
+logger = logging.getLogger(__name__)
 
 def main(args=None):
     if args is None:
@@ -35,25 +37,37 @@ This is useful for faster inference, and VLLM did not work with lora on Kaggle h
 
 
 def merge_lora(base_model_path, lora_path, output_path):
-    base_model = AutoModelForCausalLM.from_pretrained(base_model_path, torch_dtype=torch.float16)
-    tokenizer = AutoTokenizer.from_pretrained(base_model_path, trust_remote_code=True)
-    if 'llama' in base_model_path:
-        tokenizer.add_special_tokens({'pad_token': '<|pad|>'})
-        base_model.resize_token_embeddings(len(tokenizer))
+    if is_lora_path(lora_path):
+        base_model = AutoModelForCausalLM.from_pretrained(base_model_path, torch_dtype=torch.float16)
+        tokenizer = AutoTokenizer.from_pretrained(base_model_path, trust_remote_code=True)
+        if 'llama' in base_model_path:
+            tokenizer.add_special_tokens({'pad_token': '<|pad|>'})
+            base_model.resize_token_embeddings(len(tokenizer))
 
 
-    model = PeftModel.from_pretrained(base_model, lora_path)
-    merged_model = model.merge_and_unload()
-    print('Saving the merged model to the output path')
-    merged_model.save_pretrained(output_path)
+        model = PeftModel.from_pretrained(base_model, lora_path)
+        merged_model = model.merge_and_unload()
+        logger.info('Saving the merged model to the output path')
+        merged_model.save_pretrained(output_path)
+    else:
+        logger.warning('The provided lora_path does not contain a lora adapter model, it is a full model')
+        os.makedirs(output_path, exist_ok=True)
+        for filepath in glob.glob(os.path.join(lora_path, '*')):
+            dst = os.path.join(output_path, os.path.basename(filepath))
+            logger.info(f'Copying {filepath}...')
+            shutil.copy(filepath, dst)
 
     for filepath in glob.glob(os.path.join(base_model_path, '*')):
         dst = os.path.join(output_path, os.path.basename(filepath))
         if not os.path.exists(dst):
-            print('Copying', filepath)
+            logger.info(f'Copying {filepath}...')
             shutil.copy(filepath, dst)
+    logger.info('Done!')
 
-    print('Done!')
+
+def is_lora_path(lora_path):
+    return os.path.exists(os.path.join(lora_path, 'adapter_model.safetensors'))
+
 
 
 if __name__ == '__main__':
