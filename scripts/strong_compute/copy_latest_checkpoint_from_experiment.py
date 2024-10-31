aed70b2f8ac382a9f75ab463be3783375150a8dd
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2024-10-31 10:26:32 +0100

    update script

--- scripts/strong_compute/copy_latest_checkpoint_from_experiment.py
+++ scripts/strong_compute/copy_latest_checkpoint_from_experiment.py
@@ -10,12 +10,13 @@ def main(args=None):
     args = parse_args(args)
     checkpoint_paths = sorted(glob.glob(f'/root/exports/{args.experiment}/outputs/models/*/*/checkpoint-*'), key=lambda x:int(x.split('checkpoint-')[-1]))
     print(f'Found {len(checkpoint_paths)} checkpoints')
-    src_path = checkpoint_paths[-1]
-    print(f'Source path: {src_path}')
-    dst_path = '/root/models/' + src_path.split('/models/')[-1]
-    print(f'Destination path: {dst_path}')
-    shutil.copytree(src_path, dst_path)
-    print('Done!')
+    if checkpoint_paths:
+        src_path = checkpoint_paths[-1]
+        print(f'Source path: {src_path}')
+        dst_path = '/root/models/' + src_path.split('/models/')[-1]
+        print(f'Destination path: {dst_path}')
+        shutil.copytree(src_path, dst_path)
+        print('Done!')
 
 
 def parse_args(args):
