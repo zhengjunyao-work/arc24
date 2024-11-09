1d0ad7b54bf2374a463c8318228a4123edf99eb5
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2024-11-09 08:37:52 +0100

    added custom device map for qwen2-7B

--- scripts/fine-tuning.py
+++ scripts/fine-tuning.py
@@ -292,6 +292,41 @@ def get_device_map(n_gpus, model_path, device_map):
                 'model.norm': 1,
                 'model.rotary_emb': 1,
                 }
+        elif 'qwen2' in model_path.lower() and '7b' in model_path.lower():
+            logger.info('Using qwen2-1.5b-instruct custom device map')
+            device_map = {
+                'model.embed_tokens': 0,
+                'model.layers.0': 0,
+                'model.layers.1': 0,
+                'model.layers.2': 0,
+                'model.layers.3': 0,
+                'model.layers.4': 0,
+                'model.layers.5': 0,
+                'model.layers.6': 0,
+                'model.layers.7': 0,
+                'model.layers.8': 0,
+                'model.layers.9': 0,
+                'model.layers.10': 0,
+                'model.layers.11': 0,
+                'model.layers.12': 0,
+                'model.layers.13': 0,
+                'model.layers.14': 1,
+                'model.layers.15': 1,
+                'model.layers.16': 1,
+                'model.layers.17': 1,
+                'model.layers.18': 1,
+                'model.layers.19': 1,
+                'model.layers.20': 1,
+                'model.layers.21': 1,
+                'model.layers.22': 1,
+                'model.layers.23': 1,
+                'model.layers.24': 1,
+                'model.layers.25': 1,
+                'model.layers.26': 1,
+                'model.layers.27': 1,
+                'model.norm': 1,
+                'model.rotary_emb': 1,
+                'lm_head': 1}
         elif 'nano' in model_path.lower() or 'smollm' in model_path.lower():
             # quick fix for Kaggle
             logger.warning(f'Custom device map was asked, but instead balanced was returned as quick fix: {model_path}')
